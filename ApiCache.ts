99115c2a397b563250fbeec0776897002fe7a363
parkernilson <nilsopar@gmail.com>
2019-11-26 14:59:45 -0700

    moved js files to dist

--- ApiCache.ts
+++ ApiCache.ts
@@ -2,12 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { ReplaySubject, Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
+/**
+ * Api Cache is an object responsible for requesting a payload from an API endpoint,
+ * then caching it, so that multiple requests do not have to be made to access the resource
+ * throughout the program.
+ */
 export class ApiCache<DataType> {
    payload: ReplaySubject<DataType>; 
    apiEndpoint: string;
 
-   ok: boolean = false;
+   /** ok is false if there are no unresolved errors. */
+   ok: boolean = true;
+   /** true if a request has been made to the server for the payload. */
    requested: boolean = false;
+   /** true if the payload has been successfully retrieved from the server. */
    retrieved: boolean = false;
 
    constructor(apiEndpoint: string, public httpClient: HttpClient) {
@@ -15,6 +23,10 @@ export class ApiCache<DataType> {
        this.payload = new ReplaySubject<DataType>(1);
    }
 
+   /**
+    * Expose an rxjs subject of the payload, and if a request has not been made
+    * to the server yet, make a request.
+    */
    getData(): ReplaySubject<DataType> {
        if(this.ok === false) {
         //TODO: if any errors have ocurred, we need to create a new payload stream
@@ -33,6 +45,7 @@ export class ApiCache<DataType> {
            })
            this.requested = true; //we have made the request
        }
+       //expose the payload to the subscriber
        return this.payload;
    }
 }
